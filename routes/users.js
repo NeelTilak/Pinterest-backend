7106d24d3203b5265a65c69a7f0aa1a27708f03d
NeelTilak <neeltilak2.1998@gmail.com>
2023-11-12 19:21:35 +0530

    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose')
+
+mongoose.connect("mongodb://127.0.0.1:27017/Pinterest")
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ],
+  dp: {
+    type: String, // You can set a default image file
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    // required: true,
+  },
+});
+
+userSchema.plugin(plm)
+
+
+module.exports = mongoose.model('User', userSchema);
+
+
